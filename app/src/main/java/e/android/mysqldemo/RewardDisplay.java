4da15bb1cacb36f1e085102afd1dcb1254d5d05a
Joshua Pack <joshuapack1@gmail.com>
2019-07-16 21:11:38 -0400

    post request now sends JSON, updated scrollbars

--- app/src/main/java/e/android/mysqldemo/RewardDisplay.java
+++ app/src/main/java/e/android/mysqldemo/RewardDisplay.java
@@ -6,10 +6,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -57,16 +59,40 @@ public class RewardDisplay extends AppCompatActivity {
         rv.setAdapter(adapter);
         rv.setLayoutManager(new LinearLayoutManager(RewardDisplay.this));
 
-        //RewardsAsyncFetch backgroundWorker = new RewardsAsyncFetch();
-        //backgroundWorker.execute("rewards", "lol", "lol");
+        int length = 0;
+        int businessLength = businesses.length;
+        int cardsLength = selectedCards.length;
+        if(businessLength > cardsLength){
+            length = businessLength;
+        } else {
+            length = cardsLength;
+        }
 
-        for(String business: businesses){
-            for(String cCard: selectedCards){
-                RewardsAsyncFetch backgroundWorker = new RewardsAsyncFetch();
-                backgroundWorker.execute("rewards", business, cCard);
-                expectedNumResults++;
+        JSONArray jsonArray = new JSONArray();
+        for(int x=0; x<length; x++){
+            JSONObject jsonObj = new JSONObject();
+            String business = "";
+            String card = "";
+
+            if(x < businessLength){
+                business = businesses[x];
+            }
+
+            if(x < cardsLength){
+                card = selectedCards[x];
+            }
+
+            try{
+                jsonObj.put("business",business);
+                jsonObj.put("card",card);
+                jsonArray.put(jsonObj);
+            } catch (JSONException e) {
+                e.printStackTrace();
             }
         }
+
+        RewardsAsyncFetch backgroundWorker = new RewardsAsyncFetch();
+        backgroundWorker.execute("rewards", jsonArray.toString());
     }
 
     @Override
@@ -89,33 +115,30 @@ public class RewardDisplay extends AppCompatActivity {
         @Override
         protected String doInBackground(String... params) {
             String type = params[0];
-            String login_url = "http://mathwithpack.com/XZaBBaGazi28281loginFetchRewards.php";
+            String login_url = "http://mathwithpack.com/ZXyZZkMnNaTr238123PostJSONTest.php";
             if(type.equals("rewards")) {
                 try {
-                    String business = params[1];
-                    String cCard = params[2];
+                    String jsonInputString = params[1];
                     URL url = new URL(login_url);
-                    HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
+                    HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
                     httpURLConnection.setRequestMethod("POST");
+                    httpURLConnection.setRequestProperty("Content-Type", "application/json; utf-8");
                     httpURLConnection.setDoOutput(true);
                     httpURLConnection.setDoInput(true);
                     OutputStream outputStream = httpURLConnection.getOutputStream();
-                    BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
-                    String post_data = URLEncoder.encode("business","UTF-8")+"="+URLEncoder.encode(business,"UTF-8")+"&"
-                            +URLEncoder.encode("card","UTF-8")+"="+URLEncoder.encode(cCard,"UTF-8");
-                    bufferedWriter.write(post_data);
-                    bufferedWriter.flush();
-                    bufferedWriter.close();
+                    byte[] input = jsonInputString.getBytes("utf-8");
+                    outputStream.write(input, 0, input.length);
+                    outputStream.flush();
                     outputStream.close();
-                    InputStream inputStream = httpURLConnection.getInputStream();
-                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream,"iso-8859-1"));
+                    outputStream.close();
+                    BufferedReader bufferedReader = new BufferedReader(
+                            new InputStreamReader(httpURLConnection.getInputStream(), "utf-8"));
                     String result="";
                     String line;
                     while((line = bufferedReader.readLine())!= null) {
                         result += line;
                     }
                     bufferedReader.close();
-                    inputStream.close();
                     httpURLConnection.disconnect();
                     return result;
                 } catch (MalformedURLException e) {
@@ -129,21 +152,42 @@ public class RewardDisplay extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(String result) {
+            if(result.equals("")){
+                TextView resultsTV = findViewById(R.id.resultsTV);
+                resultsTV.setText("No results found :(");
+            } else{
+                try{
+                    JSONArray jsonArray = new JSONArray(result);
+                    int length = jsonArray.length();
+                    for(int i=0; i<length; i++){
+                        JSONObject obj = jsonArray.getJSONObject(i);
+                        data.add(new RewardListData(obj.getString("CardCompany"),
+                                obj.getString("Reward"),obj.getString("Business")));
+                    }
+
+                    adapter.notifyDataSetChanged();
+                    TextView resultsTV = findViewById(R.id.resultsTV);
+                    resultsTV.setText("Results:");
+                    /*
+                    JSONObject obj1 = new JSONObject(result);
+                    JSONObject obj = obj1.getJSONObject("0");
+                    String reward = obj.getString("Reward");
+                    String business = obj.getString("Business");
+                    String card = obj.getString("CardCompany");
+                    RewardListData rewardListData = new RewardListData(card, reward, business);
+                    data.add(rewardListData);
+
+                    adapter.notifyDataSetChanged();*/
+                } catch(JSONException e){
+                    e.printStackTrace();
+                }
+            }
+            /*
             numReturnedResults++;
             if(result.equals("No Results Found")) {
                 Toast.makeText(RewardDisplay.this, "No Results found for entered query", Toast.LENGTH_LONG).show();
             } else if(!result.equals("")){
                 try{
-                    /*
-                    JSONArray results = new JSONArray(result);
-                    for(int i=0; i< results.length(); i++){
-                        JSONObject obj = results.getJSONObject(i);
-                        String reward = obj.getString("Reward");
-                        String business = obj.getString("Business");
-                        String card = obj.getString("CardCompany");
-                        RewardListData rewardListData = new RewardListData(card, reward, business);
-                        data.add(rewardListData);
-                    }*/
                     resultsFound = true;
                     TextView resultsTV = findViewById(R.id.resultsTV);
                     resultsTV.setText("Results:");
@@ -168,6 +212,7 @@ public class RewardDisplay extends AppCompatActivity {
                     resultsTV.setText("No results found :(");
                 }
             }
+            */
         }
     }
 }
